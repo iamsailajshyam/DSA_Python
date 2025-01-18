972096247b1057e8bce205378a3b9455489aecbd
SAILAJ SHYAM_MCA_A_2024-26_41_UEMK <139752636+iamsailajshyam@users.noreply.github.com>
2025-01-18 15:37:23 +0530

    Create 2(18-01-25).py

---
+++ DAY-2/2(18-01-25).py
@@ -0,0 +1,14 @@
+name = input("Name: ")
+print (name)
+###################################
+light =  input("What is color of light is present to check what to do now :")
+
+if(light == "Red"):
+  print("STOP")
+elif(light == "Yellow"):
+  print("READY")
+elif(light == "Green"):
+  print("GO")
+else:
+  print("Invalid Input")
+###################################
